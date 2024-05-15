72bd39302b0407b3162de5ca14a9d42d0781b21c
kimi <smk2124@gmail.com>
2024-05-15 15:14:15 +0800

    init

---
+++ SimpleXMLValidatorLibrary/SimpleXmlValidator.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleXMLValidatorLibrary
+{
+    public class SimpleXmlValidator
+    {
+        //Please implement this method
+        public static bool DetermineXml(string xml)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
